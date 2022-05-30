ededa887e7fc084620d4b12a9e79cc46918ec29d
Salvatore Anile <salvatoreanile629@gmail.com>
2022-05-30 11:42:15 +0200

    Initial commit

---
+++ src/esercitazione1/Address.java
@@ -0,0 +1,23 @@
+package esercitazione1;
+
+public class Address {
+
+    /*
+
+    static InetAddress getByName(String host); restituisce l'indirizzo IP di un dato host
+
+    static InetAddress[] getAllByName(String host); restituisce tutti gli indirizzi IP di un dato host
+
+    static InetAddress getLocalHost(); restituisce l'indirizzo IP di localhost
+
+    byte[] getAddress(); restituisce l'indirizzo IP sotto forma di array di byte
+
+    String getHostAddress(); restituisce l'indirizzo IP sotto forma di stringa (es. "192.168.85.75")
+
+    String getHostName(); restituisce l'hostname associato all'indirizzo IP
+
+    UnknownHostException; eccezione generata quando non si riesce a trovare un host
+
+    */
+
+}//Address
---
+++ src/esercitazione1/printAddress.java
@@ -0,0 +1,29 @@
+package esercitazione1;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class printAddress {
+
+    static void printLocalAddress(){
+        try{
+            InetAddress myself = InetAddress.getLocalHost();
+            System.out.println("My name : " + myself.getHostName());
+            System.out.println("My IP address : " + myself.getHostAddress());
+
+        }catch(UnknownHostException e){
+            System.out.println("Failed to find myself");
+        }
+    }//printLocalAddress
+
+    static void printRemoteAddress(String name){
+        try{
+            InetAddress host = InetAddress.getByName(name);
+            System.out.println("Host name : " + host.getHostName());
+            System.out.println("Host IP : " + host.getHostAddress());
+        }catch(UnknownHostException e){
+            System.out.println("Failed to find the host with name " + name);
+        }
+    }//printRemoteAddress
+    
+}//printAddress
