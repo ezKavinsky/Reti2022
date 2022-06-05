802552c72db90e072224549d43915e34a3f4ff71
Salvatore Anile <salvatoreanile629@gmail.com>
2022-06-05 12:25:49 +0200

    Added ClientMain

---
+++ src/esercitazione5/Calcolatore/ClientMain.java
@@ -0,0 +1,38 @@
+package esercitazione5.Calcolatore;
+
+public class ClientMain {
+
+    /*
+            public static void main(String[] args) throws AxisFault {
+                CalcolatoreStub stud = new CalcolatoreStub();
+                try{
+                    CalcolatoreStub.Add addRequest = new CalcolatoreStub.Add();
+                    addRequest.setX(5);
+                    addRequest.addY(6);
+                    CalcolatoreStub.AddResponse addResponse = stub.add(addRequest);
+                    System.out.println("ADD: " + addResponse.get_return() );
+
+                    CalcolatoreStub.Subtract subRequest = new CalcolatoreStub.Subtract();
+                    subRequest.setX(5);
+                    subRequest.setY(6);
+                    CalcolatoreStub.SubResponse subResponse = stub.subtract(subRequest);
+                    System.out.println("SUBTRACT: " + subResponse.get_return() );
+
+                    CalcolatoreStub.Multiply multiplyRequest = new CalcolatoreStub.Multiply();
+                    multiplyRequest.setX(5);
+                    multiplyRequest.setY(6);
+                    CalcolatoreStub.MultiplyResponse multiplyResponse = stub.multiply(multiplyRequest);
+                    System.out.println("MULTIPLY: " + multiplyResponse.get_return() )
+
+                    CalcolatoreStub.Divide divideRequest = new CalcolatoreStub.Divide();
+                    divideRequest.setX(5);
+                    divideRequest.setY(6);
+                    CalcolatoreStub.DivideResponse divideResponse = stub.divide(divideRequest);
+                    System.out.println("DIVIDE: " + divideResponse.get_return() );
+            }catch(RemoteException e){
+                e.printStackTrace();
+            }
+     */
+
+
+}
